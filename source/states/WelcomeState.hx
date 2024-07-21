209c05e2bf39d3d7ffbcfe907ff33e84186bf347
Til Tjardes <til.tjardes@gmail.com>
2024-07-21 15:09:09 -0600

    welcome state fixes

--- source/states/WelcomeState.hx
+++ source/states/WelcomeState.hx
@@ -21,7 +21,7 @@ class WelcomeState extends MusicBeatState
 		bg = new FlxSprite(20);
 		add(bg);
 
-		displayText = new FlxText(0, 0, 0, '', 35);
+		displayText = new FlxText(0, 0, FlxG.width/2, '', 35);
 		displayText.setFormat(Paths.font('DigitalDisco.ttf'), 35);
 		add(displayText);
 
@@ -39,7 +39,7 @@ class WelcomeState extends MusicBeatState
 				FlxTransitionableState.skipNextTransOut = true;
 				FlxG.sound.play(Paths.sound('confirmMenu'));
 				FlxTween.tween(bg, {y: bg.y + 1000, x: bg.x + 30, angle: bg.angle + 5}, 2.5, {ease: FlxEase.backIn});
-				FlxTween.tween(displayText, {y: displayText.y + 1000, x: displayText.x - 30}, 3, { ease: FlxEase.backIn,
+				FlxTween.tween(displayText, {y: displayText.y + 1000, x: displayText.x - 30, angle: displayText.angle - 5}, 3, { ease: FlxEase.backIn,
 					onComplete: function (twn:FlxTween) {
 						FlxG.save.data.sawWelcomeState = true;
 						MusicBeatState.switchState(new TitleState());
@@ -62,30 +62,26 @@ class WelcomeState extends MusicBeatState
 	{
 		if(!isCommunityGame)
 		{
-			displayText.text = "Thank you for downloading the demo for\n
-			Sunday Night Chillin'\n\n
-			This mod took longer than expected\n
-			but here is the demo!\n\n
-			Anyways, I'm not gonna waste more time...\n\n
-
-			Hit ENTER to play!";
+			displayText.text = "Thank you for downloading the demo for Sunday Night Chillin'\n\n" +
+			"This mod took longer than expected but here is the demo!\n\n\n" +
+			"Anyways, I'm not gonna waste more time...\n\n" +
+			"Hit ENTER to play!";
 		}
 		else
 		{
-			displayText.text = "Hey CommunityGame, thanks for playing\n
-			this mod! Before the mod starts...\n 
-			I'll just be letting you know, please do NOT add\n
-			your own skins. We worked hard and don't\n
-			want another \"problem\" like we had before.\n
-			Please and thank you.\n
-			From .json, the owner.\n
-			Anyways,\n\n 
-
-			Smash that ENTER to play.";			
+			displayText.text = "Hey CommunityGame, thanks for playing this mod!\n" +
+			"Before the mod starts...\n" +
+			"I'll just be letting you know, please do NOT add your own skins.\n" +
+			"We worked hard and don't want another \"problem\" like we had before.\n" +
+			"Please and thank you.\n\n" +
+			"From .json, the owner.\n\n\n" +
+			"Anyways,\n\n" +
+			"Smash that ENTER to play.";
 		}
 
-		displayText.screenCenter();
-		displayText.x += 260;
+		displayText.updateHitbox();
+		displayText.screenCenter(Y);
+		displayText.x = FlxG.width - displayText.width - 10;
 
 		bg.loadGraphic(Paths.image(isCommunityGame ? 'welcome/community' : 'welcome/thx'));
 		bg.screenCenter(Y);
